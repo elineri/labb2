d0abfebba4f867e22340c71ee0ac49d58c796f5b
elineri <elin.ericstam@gmail.com>
2022-05-15 23:00:28 +0200

    Fixes

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -3,18 +3,33 @@ import {useState, useEffect} from "react";
 
 export default function Portfolio(){
     const [name, setName] = useState([]);
+    const [isPending, setIsPending] = useState(true); // Loading message
+
     useEffect(() => {
-        fetch('https://api.github.com/users/elineri/repos')
-        .then(res => res.json())
-        .then(data => {
-            setName((prevname) => [...prevname,
-                {
-                    name: data[0].name,
-                    description: data[0].description,
-                    html_url: data[0].html_url
-                }
-            ]);
-        });
+        setTimeout(() => {
+            fetch('https://api.github.com/users/elineri/repos')
+            .then(res => res.json())
+            .then(data => {
+                setName((prevname) => [...prevname,
+                    {
+                        name: data[1].name,
+                        description: data[1].description,
+                        html_url: data[1].html_url
+                    },
+                    {
+                        name: data[2].name,
+                        description: data[2].description,
+                        html_url: data[2].html_url
+                    },
+                    {
+                        name: data[3].name,
+                        description: data[3].description,
+                        html_url: data[3].html_url
+                    }
+                ]);
+                setIsPending(false); // Remove loading message when data had been loaded
+            });
+        }, 1000);
     },[]);
 
     return(
@@ -29,16 +44,17 @@ export default function Portfolio(){
             </nav>
             <main>
                 <h2>Min Portfolio</h2>
+                {isPending && <div>Loading...</div>} 
                 {
-                    name.map((names) => {
+                    name.map((names) => (
                         <>
                             <h3>{names.name}</h3>
                             <p>{names.description}</p>
                             <p>
-                                <a href={names.html_url}>{names.url}</a>
+                                <a href={names.html_url}>{names.html_url}</a>
                             </p>
                         </>
-                    })
+                    ))
                 }
             </main>
         </div>
--- src/index.js
+++ src/index.js
@@ -1,22 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-// import reportWebVitals from './reportWebVitals';
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <BrowserRouter>
-    <React.StrictMode>
-      <App/>
-    </React.StrictMode>
-  </BrowserRouter>, document.getElementById('root')
+    <App/>
+  </BrowserRouter>
 );
 
 
